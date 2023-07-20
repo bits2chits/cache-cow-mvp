6c86e29dc59af70c30d3cd4ff86a318c167b4eed
Julian Torp <julian.torp@gmail.com>
2023-07-20 20:29:43 +0200

    add admin code

--- __tests__/kafka.test.ts
+++ __tests__/kafka.test.ts
@@ -1,46 +1,39 @@
 import {jest} from '@jest/globals'
 import {uuidV4} from "web3-utils"
-import {createAdmin, createConsumer, createProducer} from "../src/kafka"
-import {Admin, Consumer, Producer, Message, KafkaMessage} from "kafkajs"
+import {Web3} from "web3"
+import {Producer, Message, KafkaMessage} from "kafkajs"
+import {createConsumer, createProducer} from "../src/kafka"
+import {kafkaAdminInstance} from "../src/kafka/admin"
 import {sleep} from "../src/libs/sleep"
 
 jest.setTimeout(100000)
 
+function messageToString(m: Message | KafkaMessage): string {
+  return m.value.toString()
+}
 describe('Tests Kafka', () => {
   let producer: Producer
-  let consumer: Consumer
-  let admin: Admin
   let testTopic: string
 
   beforeAll(async () => {
     testTopic = uuidV4()
-    // initialize admin
-    admin = await createAdmin()
-    await admin.connect()
-    await admin.createTopics({
-      topics: [{topic: testTopic}],
-      waitForLeaders: false
-    })
+    await kafkaAdminInstance.createTopic(testTopic)
     // initialize producer
     producer = await createProducer()
     await producer.connect()
-    // initialize consumer
-    consumer = await createConsumer({groupId: uuidV4()})
-    await consumer.connect()
-    await consumer.subscribe({topic: testTopic, fromBeginning: true})
+
   })
   afterAll(async () => {
     await producer?.disconnect()
-    await consumer?.disconnect()
-    await admin.deleteTopics({topics: [testTopic]})
-    await admin.disconnect()
-  })
-  afterEach(async () => {
-    await consumer.stop()
+    await kafkaAdminInstance.deleteTopic(testTopic)
+    await kafkaAdminInstance.disconnect()
   })
   it('should produce a kafka message', async () => {
     const messageValue = uuidV4()
     const consumedMessages: KafkaMessage[] = []
+    const consumer = await createConsumer({groupId: uuidV4()})
+    await consumer.connect()
+    await consumer.subscribe({topic: testTopic})
     await consumer.run({
       eachMessage: async ({message}) => {
         consumedMessages.push(message)
@@ -49,6 +42,7 @@ describe('Tests Kafka', () => {
     await producer.send({
       topic: testTopic,
       messages: [{
+        key: uuidV4(),
         value: messageValue
       }]
     })
@@ -57,12 +51,15 @@ describe('Tests Kafka', () => {
       await sleep(100)
     }
     expect(consumedMessages.map(({value}) => value.toString())).toContain(messageValue)
+    await consumer.disconnect()
   })
   it('should produce many kafka messages', async () => {
     const numberOfMessages = Math.floor(Math.random() * 100)
-    const producedMessages: Message[] = new Array(numberOfMessages).fill({}).map(() => ({value: uuidV4()}))
+    const producedMessages: Message[] = new Array(numberOfMessages).fill({}).map(() => ({key: uuidV4(), value: uuidV4()}))
     const consumedMessages: KafkaMessage[] = []
-
+    const consumer = await createConsumer({groupId: uuidV4()})
+    await consumer.connect()
+    await consumer.subscribe({topic: testTopic})
     await consumer.run({
       eachMessage: async ({message}) => {
         consumedMessages.push(message)
@@ -76,8 +73,16 @@ describe('Tests Kafka', () => {
     while (consumedMessages.length < producedMessages.length) {
       await sleep(100)
     }
-    expect(consumedMessages.length).toBeGreaterThanOrEqual(numberOfMessages)
-    expect(producedMessages.map(({value}) => value))
-      .toStrictEqual(consumedMessages.map(({value}) => value.toString()))
+    expect(consumedMessages.length).toEqual(numberOfMessages)
+    expect(producedMessages.map(messageToString)).toStrictEqual(consumedMessages.map(messageToString))
+    await consumer.disconnect()
+  })
+  it('should crate a topic from event signature', async () => {
+    const eventSignature = 'Event(uint256)'
+    const eventHash = (new Web3()).eth.abi.encodeEventSignature(eventSignature)
+    await kafkaAdminInstance.createTopicFromEventSignature(eventSignature)
+    const topics = await kafkaAdminInstance.listTopics()
+    expect(topics).toContain(eventHash)
+    await kafkaAdminInstance.deleteTopic(eventHash)
   })
 });
---
+++ src/kafka/admin.ts
@@ -0,0 +1,74 @@
+import {createAdmin} from "./index"
+import {Web3} from "web3"
+import {Admin} from "kafkajs"
+
+class KafkaAdmin {
+  admin: Admin
+
+  constructor() {
+    process.on('exit', async () => {
+      await this.admin?.disconnect()
+    })
+  }
+
+  async getAdminInstance(): Promise<Admin> {
+    if (!this.admin) {
+      this.admin = await createAdmin()
+      await this.admin.connect()
+    }
+    return this.admin
+  }
+
+  async createTopicFromEventSignature(eventSignature: string): Promise<void> {
+    const web3 = new Web3()
+    const eventHash = web3.eth.abi.encodeEventSignature(eventSignature)
+    await this.createTopic(eventHash)
+  }
+
+  async createTopic(topic: string): Promise<void> {
+    const admin = await this.getAdminInstance()
+    const topics = await admin.listTopics()
+    if (!topics.includes(topic)) {
+      await admin.createTopics({
+        waitForLeaders: false,
+        topics: [{
+          topic,
+          configEntries: [
+            {
+              name: "cleanup.policy",
+              value: "compact"
+            },
+            {
+              name: "retention.ms",
+              value: "-1"
+            }
+          ]
+        }]
+      })
+    }
+  }
+
+  async deleteTopic(topic: string): Promise<void> {
+    const admin = await this.getAdminInstance()
+    await admin.deleteTopics({
+      topics: [topic]
+    })
+  }
+
+  async listTopics(): Promise<string[]> {
+    const admin = await this.getAdminInstance()
+    return admin.listTopics()
+  }
+
+  async disconnect() : Promise<void> {
+    await this.admin?.disconnect()
+  }
+}
+
+export const kafkaAdminInstance: KafkaAdmin = new KafkaAdmin()
+
+
+
+
+
+
