39c32623be34ba569ae0619ccb6eb5742a072c6a
Julian Torp <julian.torp@gmail.com>
2023-08-05 17:50:17 +0200

    correct main merge

--- src/server/socket-server/socket-server.ts
+++ src/server/socket-server/socket-server.ts
@@ -45,9 +45,7 @@ export async function socketServer(
 
   priceAggregateProcessor.registerListener(uuid(), (prices: PricesMap) => {
     priceState = prices;
-    sortedPairs = Array.of(...new Set(Object.values(prices)
-      .flatMap((p) => [p.token0, p.token1])
-      .sort((a, b) => a.localeCompare(b))));
+    sortedPairs = Object.keys(prices).sort((a, b) => a.localeCompare(b)); 
     sockets.forEach((socket: Socket<SocketEvents>) => {
       socket.emit('prices', filterPrices(socket.data.filters));
       socket.emit('pairs', sortedPairs);
